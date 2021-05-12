70da32b1d4c9211b3aae1b0934f8a84579780c6f
lanyun <1684563144@qq.com>
2021-05-12 23:21:29 +0800

    开始webpack

---
+++ vue/13-webpack 的使用/01-webpack 起步/src/main.js
@@ -0,0 +1,4 @@
+import { add, mul } from './mayhUtils.js'
+
+add(1, 2);
+mul(2, 4);
---
+++ vue/13-webpack 的使用/03-webpack 的loader/src/js/main.js
@@ -0,0 +1,6 @@
+import { add, mul } from './mayhUtils.js'
+
+add(1, 2);
+mul(2, 4);
+
+require('../css/normal.css')
