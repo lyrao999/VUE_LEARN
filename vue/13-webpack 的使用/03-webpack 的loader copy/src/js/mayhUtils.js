5f7b449601c78db163096f3b54f6a5bfc4cf01aa
lanyun <1684563144@qq.com>
2021-05-13 22:15:36 +0800

    webpack loader

---
+++ vue/13-webpack 的使用/03-webpack 的loader copy/src/js/mayhUtils.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+function mul(a, b) {
+    console.log(a * b);
+}
+
+export {
+    add,
+    mul
+}
--- vue/13-webpack 的使用/03-webpack 的loader/src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { add, mul } from './mayhUtils.js'
-
-add(1, 2);
-mul(2, 4);
-
-require('../css/normal.css')
