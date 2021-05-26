b7bdc93d39c87d1a3a1a75b569591e446e157d4b
lanyun <1684563144@qq.com>
2021-05-26 22:19:25 +0800

    更新

--- vue/项目/supermarket/src/views/home/Home.vue
+++ vue/项目/supermarket/src/views/home/Home.vue
@@ -1,117 +1,134 @@
 <template>
-    <div id="home">
-        <nav-bar class="home-nav" />
-        <home-swiper :banners="banners" />
-        <home-recommend :recoms="recommends" />
-        <feature-view />
-        <tab-control class="tab-contorl" :titles="['流行', '新款', '精选']" />
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-            <li>11</li>
-            <li>12</li>
-            <li>13</li>
-            <li>14</li>
-            <li>15</li>
-            <li>16</li>
-            <li>17</li>
-            <li>18</li>
-            <li>19</li>
-            <li>20</li>
-            <li>21</li>
-            <li>22</li>
-            <li>23</li>
-            <li>24</li>
-            <li>25</li>
-            <li>26</li>
-            <li>27</li>
-            <li>28</li>
-            <li>29</li>
-            <li>30</li>
-            <li>31</li>
-            <li>32</li>
-            <li>33</li>
-            <li>34</li>
-            <li>35</li>
-            <li>36</li>
-            <li>37</li>
-            <li>38</li>
-            <li>39</li>
-            <li>40</li>
-            <li>41</li>
-            <li>42</li>
-            <li>43</li>
-            <li>44</li>
-            <li>45</li>
-            <li>46</li>
-            <li>47</li>
-            <li>48</li>
-            <li>49</li>
-            <li>50</li>
-        </ul>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav" />
+    <home-swiper :banners="banners" />
+    <home-recommend :recoms="recommends" />
+    <feature-view />
+    <tab-control class="tab-contorl" :titles="['流行', '新款', '精选']" />
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+      <li>32</li>
+      <li>33</li>
+      <li>34</li>
+      <li>35</li>
+      <li>36</li>
+      <li>37</li>
+      <li>38</li>
+      <li>39</li>
+      <li>40</li>
+      <li>41</li>
+      <li>42</li>
+      <li>43</li>
+      <li>44</li>
+      <li>45</li>
+      <li>46</li>
+      <li>47</li>
+      <li>48</li>
+      <li>49</li>
+      <li>50</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import TabControl from 'components/content/tabControl/TabControl'
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
 
-import HomeSwiper from './childCpn/HomeSwiper'
-import HomeRecommend from './childCpn/HomeRecommend'
-import FeatureView from './childCpn/FeatureView'
+import HomeSwiper from "./childCpn/HomeSwiper";
+import HomeRecommend from "./childCpn/HomeRecommend";
+import FeatureView from "./childCpn/FeatureView";
 
-import { getHomeMutilData } from 'network/home'
+import { getHomeMutilData, getHomeGoods } from "network/home";
 
 export default {
-    name: 'Home',
-    data() {
-        return {
-            banners: [],
-            recommends: []
-        }
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      data: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommend,
+    FeatureView,
+    TabControl,
+  },
+  created() {
+    this.getMutilData();
+
+    this.getGoods();
+  },
+  methods: {
+    getMutilData() {
+      getHomeMutilData().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
-    components: {
-        NavBar,
-        HomeSwiper,
-        HomeRecommend,
-        FeatureView,
-        TabControl
+    getGoods() {
+      getHomeGoods("pop", 1).then((res) => {
+        console.log(res);
+      });
     },
-    created() {
-        getHomeMutilData().then(res => {
-            this.banners = res.data.banner.list
-            this.recommends = res.data.recommend.list
-        })
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-    #home {
-        padding-top: 44px;
-    }
+#home {
+  padding-top: 44px;
+}
 
-    .home-nav {
-        position: fixed;
-        left: 0;
-        right: 0;
-        top: 0;
-        z-index: 999;
-        /* 使用css 变量 --color-tint */
-        background-color: var(--color-tint);
-        color: #fff;
-    }
+.home-nav {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+  /* 使用css 变量 --color-tint */
+  background-color: var(--color-tint);
+  color: #fff;
+}
 
-    .tab-contorl {
-        position: sticky;
-        top: 44px;
-    }
+.tab-contorl {
+  position: sticky;
+  top: 44px;
+}
 </style>
